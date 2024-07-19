0e05cb317409bbccffbdac6ec52b52faf715b73d
xiusin <826466266@qq.com>
2024-07-19 09:13:56 +0800

    练习

--- src/root.zig
+++ src/root.zig
@@ -8,8 +8,3 @@ export fn add(a: i32, b: i32) i32 {
 test "basic add functionality" {
     try testing.expect(add(3, 7) == 10);
 }
-
-// zig jetbrans
-// toolchain    /usr/local/Cellar/zig/0.13.0/bin
-// std path     /usr/local/Cellar/zig/0.13.0/lib/zig/std
-// zls path     /Users/xiusin/.bin/zls
