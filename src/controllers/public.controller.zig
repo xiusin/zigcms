4741579c323a7cbd05b2d921bdd015a5fa41af1e
xiusin <826466266@qq.com>
2024-08-05 13:46:13 +0800

    w

--- src/controllers/public.controller.zig
+++ src/controllers/public.controller.zig
@@ -6,6 +6,8 @@ const base = @import("base.fn.zig");
 pub const Public = struct {
     const Self = @This();
 
+    const ResourceBaseDir = "resources";
+
     allocator: std.mem.Allocator,
     pub fn init(allocator: std.mem.Allocator) Self {
         return .{ .allocator = allocator };
@@ -26,7 +28,6 @@ pub const Public = struct {
                     zap.Request.HttpParam.Hash_Binfile => |*file| {
                         const origin_filename = file.filename orelse return base.send_failed(self.allocator, req, "文件上传失败");
                         const data = file.data orelse return base.send_failed(self.allocator, req, "文件上传失败");
-
                         const ext = std.fs.path.extension(origin_filename);
 
                         const Md5 = std.crypto.hash.Md5;
@@ -44,38 +45,36 @@ pub const Public = struct {
                             md5[3..6],
                         }) catch return base.send_failed(self.allocator, req, "获取文件错误");
                         defer self.allocator.free(dir);
-                        std.fs.cwd().makePath(dir) catch return base.send_failed(self.allocator, req, "创建上传目录失败");
+                        var path: [1024]u8 = undefined;
+                        std.fs.cwd().makePath(std.fmt.bufPrint(path[0..], "{s}/{s}", .{
+                            ResourceBaseDir[0..],
+                            dir,
+                        }) catch return base.send_failed(
+                            self.allocator,
+                            req,
+                            "创建上传目录失败",
+                        )) catch return base.send_failed(self.allocator, req, "创建上传目录失败");
 
                         // 创建目录
-                        const savedir = std.mem.concat(self.allocator, u8, &[_][]const u8{
-                            "resources/",
-                            dir,
-                        }) catch return base.send_failed(self.allocator, req, "构建地址错误");
+                        const sd = &[_][]const u8{ ResourceBaseDir[0..], "/", dir };
+                        const savedir = std.mem.concat(self.allocator, u8, sd) catch return base.send_failed(self.allocator, req, "构建地址错误");
                         defer self.allocator.free(savedir);
 
                         // 生成相对目录
-                        const filename = std.mem.concat(self.allocator, u8, &[_][]const u8{
-                            savedir, "/",
-                            md5,     ext,
-                        }) catch return base.send_failed(self.allocator, req, "构建地址错误:LOCALFILE");
+                        const fd = &[_][]const u8{ savedir, "/", md5, ext };
+                        const filename = std.mem.concat(self.allocator, u8, fd) catch return base.send_failed(self.allocator, req, "上传失败");
                         defer self.allocator.free(filename);
 
                         var cache = true;
-                        const fullpath = std.fs.cwd().realpathAlloc(
-                            self.allocator,
-                            filename,
-                        ) catch return base.send_failed(self.allocator, req, "生成对象地址错误:FULLPATH");
-                        defer self.allocator.free(fullpath);
-
                         const url = std.mem.concat(
                             self.allocator,
                             u8,
-                            &[_][]const u8{ "https://dev.xiusin.cn/", filename[10..] },
+                            &[_][]const u8{ "https://dev.xiusin.cn/", filename[ResourceBaseDir.len..] },
                         ) catch return base.send_failed(self.allocator, req, "生成对象地址错误:URL");
                         defer self.allocator.free(url);
 
                         // 判断文件是否存在, statFile 必须是绝对路径
-                        _ = std.fs.cwd().statFile(fullpath) catch |err| {
+                        _ = std.fs.cwd().statFile(filename) catch |err| {
                             std.log.debug("{any}", .{err});
                             std.fs.cwd().makePath(savedir) catch return base.send_failed(self.allocator, req, "创建上传目录失败");
                             std.fs.cwd().writeFile(.{
