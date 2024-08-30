2da1bee9e61ddf20d9dde5a9e4e56c4174e02979
xiusin <826466266@qq.com>
2024-08-30 18:31:27 +0800

    add

--- src/controllers/controllers.zig
+++ src/controllers/controllers.zig
@@ -3,3 +3,4 @@ pub const Menu = @import("menu.controller.zig");
 pub const Public = @import("public.controller.zig");
 pub const Setting = @import("setting.controller.zig");
 pub const Generic = @import("generic.controller.zig");
+pub const Github = @import("github.controller.zig");
